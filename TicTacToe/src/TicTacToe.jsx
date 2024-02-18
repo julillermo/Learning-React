fd593020be7d4769b3995b7b3b6b3fd4f24493d4
TulioG-Mint <julius.guillermo.work@gmail.com>
2024-02-18 17:46:03 +0800

    completed tic-tac-toe sample problem

--- TicTacToe/src/TicTacToe.jsx
+++ TicTacToe/src/TicTacToe.jsx
@@ -1,83 +1,159 @@
 import { useState, useEffect, useRef } from 'react';
 
-function RestartButton(){
-  return(
-    <button >
-      Restart Game
-    </button>
-  )
-}
-
 function Square({ gridId, gridRefAccess, isGameFinished, playerPiece, changePlayer }) {
-  const [isOccupied, setIsOccupied] = useState('');
+  const [occupyingPiece, setOccupyingPiece] = useState('_');
   const playerPieceLocal = playerPiece;
-// ENDED HERE
+
   function handleClick(){
-    if (isOccupied === ''){
-      setIsOccupied(playerPieceLocal);
-      gridRefAccess[gridId[0]][gridId[1]] = playerPieceLocal;
-      // console.log(gridId[0]);
-      // console.log(gridId[1]);
+    if (occupyingPiece === '_' && isGameFinished === false){
+      gridRefAccess[gridId[0]][gridId[1]] = playerPieceLocal; // the piece placed in the calculation grid
+      setOccupyingPiece(gridRefAccess[gridId[0]][gridId[1]]); // the piece rendered by React
       console.log(gridRefAccess);
     }
     changePlayer( (playerPieceLocal === 'X') ? 'O' : 'X'  )
   }
 
   return (
-    <button className="square" onClick={
-      (!gameStatus) ? handleClick}>
-      {isOccupied}
+    <button className="square" onClick={handleClick}>
+      {occupyingPiece} <br/>
+      {/* {gridId[0]}{gridId[1]} */}
     </button>
+  )
+}
+
+function WinnerAnnoucement({ winnerPiece }){
+  return (
+    <h2>
+      {winnerPiece} is the winner!
+    </h2>
   )
 }
 
 export default function TicTacToe() {
   const [currentPlayerPiece, setCurrentPlayerPiece] = useState('X')
-  const [isGameFinished, setIsGameStatus] = useState(false);
+  const [isGameFinished, setIsGameFinished] = useState(false);
+  const [winner, setWinner] = useState('');
   const gridRef = useRef(
     [
-      ['','',''],
-      ['','',''],
-      ['','','',]
+      ['_','_','_'],
+      ['_','_','_'],
+      ['_','_','_'],
     ]
   )
 
+  // Checking if a player has won
   useEffect(() => {
     let xPieceSuccessiveCount = 0;
     let oPieceSuccessiveCount = 0;
 
     function endGame(winnerPiece){
       console.log(`${winnerPiece} player won!`)
+      setWinner(winnerPiece);
     }
 
-    /* Check for row completion */
-    for (let y=0; y < gridRef.current[0].length; y++){
+    if (isGameFinished === false){
+      /* Checking for the rows for 3 consecutive pieces */
+      for (let y=0; y < gridRef.current[0].length; y++){
+        for (let x=0; x < gridRef.current.length; x++){
+          if ( gridRef.current[y][x] === 'X' ){
+            xPieceSuccessiveCount += 1;
+          } else if ( gridRef.current[y][x] === 'O' ){
+            oPieceSuccessiveCount += 1;
+          }
+        }
+  
+        if (xPieceSuccessiveCount >= 3){
+          endGame('X');
+          setIsGameFinished(true);
+        } 
+        if (oPieceSuccessiveCount >= 3) {
+          endGame('O');
+          setIsGameFinished(true);
+        }
+  
+        xPieceSuccessiveCount = 0;
+        oPieceSuccessiveCount = 0;
+      }
+
+      /* Checking for the columns for 3 consecutive pieces */
       for (let x=0; x < gridRef.current.length; x++){
+        for (let y=0; y < gridRef.current[0].length; y++){
+          if ( gridRef.current[y][x] === 'X' ){
+            xPieceSuccessiveCount += 1;
+          } else if ( gridRef.current[y][x] === 'O' ){
+            oPieceSuccessiveCount += 1;
+          }
+        }
+  
+        if (xPieceSuccessiveCount >= 3){
+          endGame('X');
+          setIsGameFinished(true);
+        } 
+        if (oPieceSuccessiveCount >= 3) {
+          endGame('O');
+          setIsGameFinished(true);
+        }
+  
+        xPieceSuccessiveCount = 0;
+        oPieceSuccessiveCount = 0;
+      }
+
+      /* Checking for the one of the diagonals for 3 consecutive pieces */
+      for (let y=0, x=0; y < gridRef.current[0].length; x++, y++){
         if ( gridRef.current[y][x] === 'X' ){
           xPieceSuccessiveCount += 1;
         } else if ( gridRef.current[y][x] === 'O' ){
           oPieceSuccessiveCount += 1;
         }
 
-      }
-      if (xPieceSuccessiveCount >= 3){
-        endGame('X');
-      } 
-      if (oPieceSuccessiveCount >= 3) {
-        endGame('O');
+        if (xPieceSuccessiveCount >= 3){
+          endGame('X');
+          setIsGameFinished(true);
+        } 
+        if (oPieceSuccessiveCount >= 3) {
+          endGame('O');
+          setIsGameFinished(true);
+        }
       }
 
       xPieceSuccessiveCount = 0;
       oPieceSuccessiveCount = 0;
+
+      /* Checking for the one of the diagonals for 3 consecutive pieces */
+      for (let y=0, x=2; y < gridRef.current[0].length; x--, y++){
+        if ( gridRef.current[y][x] === 'X' ){
+          xPieceSuccessiveCount += 1;
+        } else if ( gridRef.current[y][x] === 'O' ){
+          oPieceSuccessiveCount += 1;
+        }
+
+        // console.log(`[${y}] [${x}] = ${gridRef.current[y][x]}`);
+        // console.log(`xPeice Count: ${xPieceSuccessiveCount}`);
+        // console.log(`oPeice Count: ${oPieceSuccessiveCount}`);
+
+        if (xPieceSuccessiveCount >= 3){
+          endGame('X');
+          setIsGameFinished(true);
+        } 
+        if (oPieceSuccessiveCount >= 3) {
+          endGame('O');
+          setIsGameFinished(true);
+        }
+      }
+
+      xPieceSuccessiveCount = 0;
+      oPieceSuccessiveCount = 0;
+
     }
-
-
   })
 
   return (
     <>
-      <h1>Tic-Tac-Toe here</h1>
+      <h1>Tic-Tac-Toe</h1>
       <br /><hr /><br />
+
+      {winner !== '' && <WinnerAnnoucement winnerPiece={winner} />}
+      <br />
 
       <Square gridId={[0,0]} gridRefAccess={gridRef.current} isGameFinished={isGameFinished} playerPiece={currentPlayerPiece} changePlayer={setCurrentPlayerPiece} />
       <Square gridId={[0,1]} gridRefAccess={gridRef.current} isGameFinished={isGameFinished} playerPiece={currentPlayerPiece} changePlayer={setCurrentPlayerPiece} />
@@ -92,7 +168,12 @@ export default function TicTacToe() {
       <Square gridId={[2,2]} gridRefAccess={gridRef.current} isGameFinished={isGameFinished} playerPiece={currentPlayerPiece} changePlayer={setCurrentPlayerPiece} />
       <br />
       <br />
-      <RestartButton />
+
+      <form>
+        <button type="submit">
+          Restart
+        </button>
+      </form>
     </>
   )
 }
